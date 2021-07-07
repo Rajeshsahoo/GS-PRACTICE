2a8ca6f1d07e549259465163a4ff2954402f7eae
Rajeshkumar_Sahoo <Rajeshkumar_Sahoo@EPINBANW0140>
2021-07-07 12:02:14 +0530

    updated

--- GS-PRACTICE/src/main/java/com/gspractice/miscellaneous/CountLengthOfCycle.java
+++ GS-PRACTICE/src/main/java/com/gspractice/miscellaneous/CountLengthOfCycle.java
@@ -3,25 +3,30 @@ package com.gspractice.miscellaneous;
 public class CountLengthOfCycle {
 
 	public static void main(String[] args) {
-        System.out.println(countLengthofcycle(new int[]{1,0},0));
-        System.out.println(countLengthofcycle(new int[]{1,2,0},0));
-        System.out.println(countLengthofcycle(new int[]{1,2,1,0},0));
+		System.out.println(countLengthOfCycle(new int[]{1, 0}, 0));
+        System.out.println(countLengthOfCycle(new int[]{1, 2, 0}, 0));
+        System.out.println(countLengthOfCycle(new int[]{1, 2, 3}, 0));
     }
 
-    private static int countLengthofcycle(int[] arr, int start) {
-        int count  = 1;
-        while(arr[start]>0){
-            if(start>= arr.length)return -1;
-            int val  = arr[start];
-            if(val<0){
+    private static int countLengthOfCycle(int[] arr, int startIndex) {
+    	if (startIndex>=arr.length) {
+            return -1;
+        }
+
+        int N = arr.length;
+        boolean[] visited = new boolean[N];
+        int current = startIndex;
+        int c=0;
+        while (!visited[current]) {
+            visited[current]=true;
+            current = arr[current];
+            if (current>=N) {
                 return -1;
             }
-            arr[start]*=-1;
-            start = val;
-            count++;
+            c++;
         }
-        return count;
 
+        return c;
 
     }
 
